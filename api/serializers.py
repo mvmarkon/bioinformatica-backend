8527c3f041e7de06293667c0e1bd8c0a6d874297
Mariano Markon <mariano@camba.coop>
2020-06-18 01:14:17 -0300

    fix serializador

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import FastaEntry
 
-class FastaEntrySerializer(serializers.HyperlinkedModelSerializer):
+class FastaEntrySerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = FastaEntry
